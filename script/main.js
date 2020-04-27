8ac7beb2acf4a2de1aa46056be78c82a94e38980
Duc Thang Vu <vuducthang91@gmail.com>
2020-04-27 14:46:06 +0200

    completed

---
+++ script/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+    $('.box').click(function() {
+        self = $(this)
+
+        $.ajax({
+            url: 'https://flynn.boolean.careers/exercises/api/random/int',
+            method: 'GET',
+            
+            success: function(data) {
+                data.response <= 5 ? self.addClass('yellow') : self.addClass('green')
+
+                self.text(data.response)
+            },
+            
+            error: () => console.log('Error')
+        })
+    })
+
+})
+
